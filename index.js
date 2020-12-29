73d0ff2d547bdd8ad16cda11fb64bc0842eaf10d
mooner1022 <moonmk10221@gmail.com>
2020-12-29 15:03:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ io.on('connection',function(socket) {
         console.log('newMessage triggered')
         try {
             const messageData = JSON.parse(data)
-            const userName = room_data.userName;
+            const userName = messageData.userName;
             const messageContent = messageData.messageContent
             const roomName = messageData.roomName
     
